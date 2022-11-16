6760bdf5496e654bb91cba51cbcde448993197e4
Christian <chr.troller@gmail.com>
2022-11-16 17:53:57 +0100

    Implement inter-addon communication

--- src/IncendioLoot.lua
+++ src/IncendioLoot.lua
@@ -1,13 +1,27 @@
 local addonName, addon = ...
-IncendioLoot = LibStub("AceAddon-3.0"):NewAddon( "IncendioLoot", "AceConsole-3.0", "AceEvent-3.0" )
-_G[ addonName ] = IncendioLoot
-
+IncendioLoot = LibStub("AceAddon-3.0"):NewAddon("IncendioLoot",
+                                                "AceConsole-3.0", "AceComm-3.0",
+                                                "AceSerializer-3.0")
+_G[addonName] = IncendioLoot
+local AceConsole = LibStub("AceConsole-3.0")
+--[[
+    Init
+]] --
+function IncendioLoot:OnEnable() LibStub("AceComm-3.0"):Embed(IncendioLoot) end
 
 --[[
     Global events
-]]--
+]] --
 IncendioLoot.EVENTS = {
     EVENT_LOOT_LOOTED = "IL.LC.LootLooted", -- whenever a member loots an item
     EVENT_LOOT_VOTE_CAST = "IL.LC.LootVoteCast", -- whenever a loot council member votes on an item
     EVENT_LOOT_DISTRIBUTED = "IL.LC.LootDistributed" -- whenever the council distributes an item
 }
+
+IncendioLoot.LootUtil = {}
+function IncendioLoot.LootUtil.SendLootEvent(self, item, looter, encounter)
+    local data = {item = item, looter = looter, encounter = encounter}
+    local s = IncendioLoot:Serialize(data)
+    IncendioLoot:SendCommMessage(IncendioLoot.EVENTS.EVENT_LOOT_LOOTED, s,
+                                 "GUILD")
+end
--- src/LootCouncil.lua
+++ src/LootCouncil.lua
@@ -1,27 +1,26 @@
+LoadAddOn("LibCompress")
+
 local addonName, addon = ...
 local IncendioLoot = _G[addonName]
+local LootCouncil = IncendioLoot:NewModule("LootCouncil",
+                                                      "AceConsole-3.0",
+                                                      "AceEvent-3.0",
+                                                      "AceComm-3.0",
+                                                      "AceSerializer-3.0")
 local AceConsole = LibStub("AceConsole-3.0")
-IncendioLoot.LootCouncil = {}
+local LibCompress = LibStub:GetLibrary("LibCompress")
 
 --[[
     Event handling
 ]] --
-
-function HandleLootLootedEvent()
-    -- TBD
+function HandleLootLootedEvent(prefix, str, distribution, sender)
+    local _, data = LootCouncil:Deserialize(str)
+    AceConsole:Print(data.looter)
 end
 
-function HandleLootVoteCastEvent()
-    -- TBD
-end
-
-function HandleLootDistributedEvent()
-    -- TBD
-end
-
-IncendioLoot:RegisterMessage(IncendioLoot.EVENTS.EVENT_LOOT_LOOTED,
+function LootCouncil:OnEnable()
+    LibStub("AceComm-3.0"):Embed(LootCouncil)
+    LootCouncil:RegisterComm(IncendioLoot.EVENTS.EVENT_LOOT_LOOTED,
                              HandleLootLootedEvent)
-IncendioLoot:RegisterMessage(IncendioLoot.EVENTS.EVENT_LOOT_VOTE_CAST,
-                             HandleLootVoteCastEvent)
-IncendioLoot:RegisterMessage(IncendioLoot.EVENTS.EVENT_LOOT_DISTRIBUTED,
-                             HandleLootDistributedEvent)
+    IncendioLoot.LootUtil:SendLootEvent("someItem", "somePlayer", "someBoss")
+end
