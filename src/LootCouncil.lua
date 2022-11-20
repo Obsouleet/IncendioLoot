b331004af01d7673aaab9fd9edda24ea5ebd7297
Christian <chr.troller@gmail.com>
2022-11-20 21:54:58 +0100

    GUI Magick failure

--- src/LootCouncil.lua
+++ src/LootCouncil.lua
@@ -1,21 +1,13 @@
-LoadAddOn("LibCompress")
-
 local addonName, addon = ...
 local IncendioLoot = _G[addonName]
 local LootCouncil = IncendioLoot:NewModule("LootCouncil", "AceConsole-3.0",
                                            "AceEvent-3.0", "AceComm-3.0",
                                            "AceSerializer-3.0")
 local AceConsole = LibStub("AceConsole-3.0")
-local LibCompress = LibStub:GetLibrary("LibCompress")
 
 --[[
     Event handling
 ]] --
-local function HandleLootLootedEvent(prefix, str, distribution, sender)
-    local _, data = LootCouncil:Deserialize(str)
-    AceConsole:Print(data)
-end
-
 local function HandleLootVoteCastEvent(prefix, str, distribution, sender)
     local _, data = LootCouncil:Deserialize(str)
     AceConsole:Print(data)
@@ -33,13 +25,10 @@ end
 
 function LootCouncil:OnInitialize()
     LibStub("AceComm-3.0"):Embed(LootCouncil)
-    LootCouncil:RegisterComm(IncendioLoot.EVENTS.EVENT_LOOT_LOOTED,
-                             HandleLootLootedEvent)
     LootCouncil:RegisterComm(IncendioLoot.EVENTS.EVENT_LOOT_VOTE_CAST,
                              HandleLootVoteCastEvent)
     LootCouncil:RegisterComm(IncendioLoot.EVENTS.EVENT_LOOT_DISTRIBUTED,
                              HandleLootDistributedEvent)
     LootCouncil:RegisterComm(IncendioLoot.EVENTS.EVENT_LOOT_SESSION_STARTED,
                              HandleLootSessionStartedEvent)
-    IncendioLoot.LootUtil:SendLootEvent("someItem", "somePlayer", "someBoss")
 end
