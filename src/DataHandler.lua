c810a55bf759ad5adaf26c9211e78375a154ce24
Incrd <gamedev@incrd.de>
2023-01-03 22:28:34 +0100

    minor bugfix

--- src/DataHandler.lua
+++ src/DataHandler.lua
@@ -2,7 +2,7 @@ local addonName, addon = ...
 local IncendioLoot = _G[addonName]
 local DataHandler = IncendioLoot:NewModule("DataHandler", "AceEvent-3.0", "AceSerializer-3.0", "AceConsole-3.0")
 local LootTable 
-local VoteData 
+local VoteData = {}
 local MasterLooter 
 local ExternalMasterLooters = {}
 local SessionActive
